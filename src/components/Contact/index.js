4c6903aa81f599279360e6e20a13fc7eefe848e2
Chris Ruiz <chrispruiz@att.net>
2021-07-26 21:36:46 -0500

    updated contact me form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -37,28 +37,24 @@ function ContactForm() {
   };
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
+    <section class="contact">
+            <ul>
+              <li>
+              <p>Business inquiries / Contact: 
+              <a href="mailto:chrispruiz@att.net">Chrispruiz@att.net</a>
+              </p>
+              </li>
+              <li>
+                <p>Check out my Github at:
+                <a href="https://github.com/Chrispruiz">Chrispruiz</a>
+                </p>
+              </li>
+              <li>
+                <p>Connect with me on
+                <a href="https://linkedin.com/in/chrispruiz">Linkedin</a>
+                </p>
+              </li>
+            </ul>
     </section>
   );
 }
